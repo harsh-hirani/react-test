4f3ad23f2c9b5b7e63e5c8ed5e110d1c216314ca
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-07 18:57:10 +0530

    props 2 done

--- src/index.js
+++ src/index.js
@@ -13,11 +13,56 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 function Welcome(props) {
   return <h1>welcome, {props.name}</h1>
 }
+function Avtar(p) {
+  console.log(p);
+  return (
+    <>
+      <img src={p.user.link}/>{/* p is prop and user is object of p becuase in p can be other as well */}
+      <Welcome name={p.user.name} />
+    </>
+
+  );
+}
+function formateDate(date){
+  return date.toLocaleDateString();
+}
+
+const Comment = (props) => {
+  return (
+    <>
+      <div className="comment">
+        <div className="UserInfo">
+          <Avtar user={myobject}/>
+          {/* passing a object */}
+          <div className="UserInfo-frame">
+            {props.user.name}
+          </div>
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formateDate(props.date)}
+        </div>
+      </div>
+    </>
+  );
+}
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  user: {
+    name: 'Hello Kitty',
+    link: 'http://placekitten.com/g/64/64'
+  }
+};
+const myobject= {
+  name: 'Hello Kitty',
+  link: 'http://placekitten.com/g/64/64'
+};
 root.render(
   <div>
-    <Welcome name="harsh"/>
-    <Welcome name="rahul"/>
-    <Welcome name="sahil"/>
+    <Comment  text={comment.text} date={comment.date} user={comment.user}/>
   </div>
 );
 // If you want to start measuring performance in your app, pass a function
