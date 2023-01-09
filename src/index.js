fac29a58c96038c573c6579a6d1fc40c019fa784
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-09 12:19:08 +0530

    list and key 1

--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
+class Page extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
 
+    }
+    arry = [0,454,5545,54,5,545,4];
+    render() {
+        return (
+            <>
+            {this.arry.map((e)=>{return(<li key={e.toString()}>{e+1}</li>)})}
+            </>
+        );
+    }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function WarningBanner({warn}) {
-  if (!warn) {
-    return null;
-  }
-  return (
-    <div>warning</div>
-  )
-}
-class Page extends React.PureComponent{
-  constructor(props){
-    super(props);
-    this.state = {warning:true}
-  }
-  click = () => {
-      this.setState(myState => ({
-        warning: !myState.warning
-      }));
-  }
-  render(){
-    return (
-      <div>
-        <WarningBanner warn={this.state.warning} />
-        <button onClick={this.click}>
-          {this.state.warning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    )
-  }
-}
 root.render(
   <>
     <Page/>
