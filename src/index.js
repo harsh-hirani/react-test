ba266027d491722d72153a16e88f2fd0ce91adf2
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-25 22:03:17 +0530

    form 1.6

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-function Item (props) {
-    const value = props.value;
-    return(
-        <li key={value.toString()}>
-            {value}
-        </li>
-    )
-}
-function List(props) {
-    const nums = props.nums;
-    const items = nums.map((umber)=>{
-        return <Item value={umber}/>
-    })
-    return(
-        <ul>
-            {items}
-        </ul>
-    )
-}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let arry = [1,2,3,4,5];
 root.render(
-  <>
-    <List nums={arry} />
-  </>
+    <>
+        <form>
+            <lable>
+                Name:
+                <input type="text" name="name" />
+            </lable>
+            <input type="submit" value="submit" />
+        </form>
+    </>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
