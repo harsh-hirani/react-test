397e9dc6ef448327c69f2d7ab8cec58ae798cbe2
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-25 22:25:02 +0530

    form 2

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+class NameForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value:'please write..'};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({value:event.target.value})
+    }
+    handleSubmit(event){
+        alert("an eassy was :"+this.state.value);
+        event.preventDefault();
+    }
+    render(){
+        return (
+            <>
+            <form onSubmit={ this.handleSubmit}>
+                <lable>
+                    Eassy:
+                    <textarea value={this.state.value} onChange={ this.handleChange} />
+                </lable>
+                <input type="submit" value="submit" />
+            </form>
+            </>
+        );
+    }
+}
 root.render(
     <>
-        <form>
-            <lable>
-                Name:
-                <input type="text" name="name" />
-            </lable>
-            <input type="submit" value="submit" />
-        </form>
+        <NameForm/>
     </>
 );
 // If you want to start measuring performance in your app, pass a function
